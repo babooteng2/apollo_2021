07654c7f36b5c1e898f34512e5ed16686c194461
Tom Kim <babooteng2@gmail.com>
2021-08-17 02:41:27 +0900

    #1.1 GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,21 @@
-const Home = () => <h1>Home</h1>;
+import { gql, useQuery } from "@apollo/client";
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
+const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (error) return error.message;
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movies) {
+    return data.movies.map((m) => <h1>{m.title}</h1>);
+  }
+};
 export default Home;

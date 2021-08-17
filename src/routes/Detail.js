32b17c6e07cc4e6753720beb085bb84e8a9ccd85
Tom Kim <babooteng2@gmail.com>
2021-08-17 22:55:00 +0900

    #1.2 GET MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,2 +1,35 @@
-const Detail = () => "Detail";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+const Detail = () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: +id },
+  });
+  console.log(loading, data);
+  if (loading) {
+    return "loading...";
+  }
+  if (data && data.movie) {
+    const { title, medium_cover_image, description_intro } = data.movie;
+    return (
+      <>
+        <div>{title}</div>
+        <img src={medium_cover_image} alt="" />
+        <p>{description_intro}</p>
+      </>
+    );
+  }
+};
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,8 @@
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
+import Movie from "../components/Movie";
+import styled from "styled-components";
+
 const GET_MOVIES = gql`
   {
     movies {
@@ -8,14 +12,60 @@ const GET_MOVIES = gql`
     }
   }
 `;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
 const Home = () => {
   const { loading, error, data } = useQuery(GET_MOVIES);
   if (error) return error.message;
-  if (loading) {
-    return "loading...";
-  }
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.title}</h1>);
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2021</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m) => (
+          <Movie key={m.id} id={m.id}>
+            {m.title}
+          </Movie>
+        ))}
+    </Container>
+  );
 };
 export default Home;
